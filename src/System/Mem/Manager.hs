3122dd5363cb8b0895918c976adac03007b6e752
Edward Kmett <ekmett@gmail.com>
2015-09-16 00:19:15 -0400

    we do nothing untoward and you can't do anything particularly unsafe with this API. Flag it Trustworthy

--- src/System/Mem/Manager.hs
+++ src/System/Mem/Manager.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE Trustworthy #-}
 {-# OPTIONS_GHC -fno-cse -fno-full-laziness -fno-float-in #-}
 
 -----------------------------------------------------------------------------
