a64505997db233dfb69300ddfc8f441ee87029dd
Edward Kmett <ekmett@gmail.com>
2015-09-15 21:09:22 -0400

    move from System.RSS to System.Mem.RSS

--- src/System/Mem/RSS.hs
+++ src/System/Mem/RSS.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-module System.RSS 
-  ( currentRSS
-  , peakRSS
+module System.Mem.RSS 
+  ( currentResidentSetSize
+  , peakResidentSetSize
   ) where
 
 import Foreign.C.Types
 
 -- | Retrieve the current resident set size for the currently executing program.
-foreign import ccall "getCurrentRSS" currentRSS :: IO CSize
+foreign import ccall "getCurrentRSS" currentResidentSetSize :: IO CSize
 
 -- | Retrieve the current peak resident set size for the currently executing program.
-foreign import ccall "getPeakRSS" peakRSS :: IO CSize
+foreign import ccall "getPeakRSS" peakResidentSetSize :: IO CSize
