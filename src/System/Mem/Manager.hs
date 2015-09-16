a92dedba892d19c63ec7609d42ea6cd05bab5b19
Edward Kmett <ekmett@gmail.com>
2015-09-15 21:54:28 -0400

    add a selfishManager and tools for tracking the number of cumulative managed collections thus far

--- src/System/Mem/Manager.hs
+++ src/System/Mem/Manager.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# OPTIONS_GHC -fno-cse -fno-full-laziness -fno-float-in #-}
 module System.Mem.Manager
   ( currentResidentSetSize
   , peakResidentSetSize
   , hardPageFaults
   , checkMemoryPressure
+  , selfishManager
+  , cumulativeManagedCollections
   ) where
 
+import Control.Concurrent
+import Control.Monad
 import Data.IORef
 import Foreign.C.Types
 import System.IO.Unsafe
+import System.Mem
 
 -- | Retrieve the current resident set size for the currently executing program.
 foreign import ccall "getCurrentRSS" currentResidentSetSize :: IO CSize
@@ -36,3 +42,21 @@ checkMemoryPressure = do
   return $! case old of
     NoManagerState -> False
     ManagerState oldRss oldFaults -> newRss < oldRss || newFaults >= oldFaults + 10
+
+managedCollections :: IORef Int
+managedCollections = unsafePerformIO $ newIORef 0
+{-# NOINLINE managedCollections #-}
+
+-- | Run a background thread that checks for memory pressure from the Host OS and kickstarts a GC.
+selfishManager :: IO ThreadId
+selfishManager = forkIO $ forever $ do
+  b <- checkMemoryPressure
+  when b $ do
+    performMajorGC
+    modifyIORef' managedCollections (+1)
+  threadDelay 50000
+
+-- | Return the total number of managed collections that the GC manager has forced.
+cumulativeManagedCollections :: IO Int
+cumulativeManagedCollections = readIORef managedCollections
+{-# INLINE cumulativeManagedCollections #-}
